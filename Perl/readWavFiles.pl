515cda10d4c55fb2a21377dd62f3a4e28015f859
Jeremy 'LeJyBy' Barbay <jeremy@barbay.cl>
2016-05-27 10:27:39 -0400

    Added tag of last modification date.

--- Perl/readWavFiles.pl
+++ Perl/readWavFiles.pl
@@ -31,7 +31,7 @@ use File::stat;            # To get the time and size of a file
 
 
 print "# Perl Script to process audionotes from a jukebox SONY.\n";
-print "# by Jeremy Barbay\n\n";c
+print "# by Jeremy Barbay\n\n";
 print "# Version last modified on [2016-05-27 Fri] \n\n";
     
     
